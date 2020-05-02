f407d6ccaabc57fc7f008c0acf603b524a0c042c
Jan Schmidt <jan@centricular.com>
2020-05-02 12:51:45 +1000

    dump fw blocks

--- test.c
+++ test.c
@@ -3,6 +3,7 @@
 #include <stdint.h>
 #include <string.h>
 #include <stdbool.h>
+#include <stdlib.h>
 #include <signal.h>
 #include <unistd.h>
 #include <time.h>
@@ -11,6 +12,8 @@
 #define FEATURE_BUFFER_SIZE 64
 
 bool done = false;
+
+static int sleep_us (uint64_t usec);
 
 static void
 sigint_handler (int signum)
@@ -61,6 +64,108 @@ static int get_report(hid_device *hid, char id, unsigned char *buff, int length)
   buff[0] = id;
   clear_buff(&buff[1], length-1);
   return hid_get_feature_report(hid, buff, length);
+}
+
+static int
+read_one_fw_block (hid_device *dev, uint8_t block_id, uint32_t pos, uint8_t read_len, uint8_t *buf)
+{
+  unsigned char req[64] = { 0x4a, 0x00, };
+  int ret, loops = 0;
+  bool send_req = true;
+
+  req[2] = block_id;
+
+  do {
+    if (send_req) {
+      /* FIXME: Little-endian code: */
+      * (uint32_t *)(req + 3) = pos;
+      req[7] = read_len;
+      ret = hid_send_feature_report(dev, req, 64);
+      if (ret < 0) {
+        printf ("Report 74 SET failed\n");
+        return ret;
+      }
+    }
+
+    ret = get_report(dev, 0x4A, buf, 64);
+    if (ret < 0) {
+      printf ("Report 74 GET failed\n");
+      return ret;
+    }
+    /* Loop until the result matches the address we asked for and
+     * the 2nd byte == 0x00 (0x1 = busy or req ignored?), or 20 attempts have passed */
+    if (memcmp (req, buf, 7) == 0)
+      break;
+
+    /* Or if the 2nd byte of the return result is 0x1, the read is being processed,
+     * don't send the req again. If it's 0x00, we seem to need to re-send the request  */
+    send_req = (buf[1] == 0x00);
+
+    sleep_us (2000);
+  } while (loops++ < 20);
+
+  if (loops > 20)
+    return -1;
+
+  return ret;
+}
+
+static int
+dump_fw_block (hid_device *dev, uint8_t block_id)
+{
+  char label[64];
+  uint32_t pos = 0x00, block_len;
+  unsigned char buf[64] = { 0x4a, 0x00, };
+  unsigned char *outbuf;
+  size_t total_read = 0;
+  uint64_t checksum;
+  int ret;
+
+  ret = read_one_fw_block (dev, block_id, 0, 0xC, buf);
+  if (ret < 0) {
+    printf ("Failed to read fw block %02x header\n", block_id);
+    return ret;
+  }
+
+  /* The block header is 12 bytes. 8 byte checksum, 4 byte size? */
+  checksum = *(uint64_t *)(buf + 8);
+  block_len = *(uint32_t *)(buf + 16);
+
+  if (block_len < 0xC || block_len == 0xFFFFFFFF)
+    return 0; /* Invalid block */
+
+  printf ("FW Block %02x Header. Checksum(?) %08lx len %d\n", block_id, checksum, block_len);
+
+  /* Copy the contents of the fw block, minus the header */
+  outbuf = malloc (block_len);
+  total_read = 0x0;
+
+  for (pos = 0x0; pos < block_len; pos += 56) {
+    uint8_t read_len = 56;
+    if (pos + read_len > block_len)
+      read_len = block_len - pos;
+
+    ret = read_one_fw_block (dev, block_id, pos + 0xC, read_len, buf);
+    if (ret < 0) {
+      printf ("Failed to read fw block %02x at pos 0x%08x len %d\n", block_id, pos, read_len);
+      break;
+    }
+    memcpy (outbuf + total_read, buf + 8, read_len);
+    total_read += read_len;
+  }
+
+  if (total_read > 0) {
+    if (total_read < block_len) {
+      printf ("Short FW read - only read %lu bytes of %u\n",
+         total_read, block_len);
+    }
+    sprintf (label, "FW Block %02x", block_id);
+    printBuffer(label, outbuf, total_read);
+  }
+
+  free (outbuf);
+
+  return ret;
 }
 
 static void
@@ -122,7 +227,7 @@ static uint64_t ohmd_monotonic_get()
   return ticks;
 }
 
-int sleep_us (uint64_t usec)
+static int sleep_us (uint64_t usec)
 {
   struct timespec ts;
   int res;
@@ -227,6 +332,7 @@ int main() {
   int b = get_report(hid_hmd, 0x06, buff, sizeof(devInfo));
   if (b < sizeof(devInfo)) {
     printf("Failed to read %d of device info\n", (int) sizeof(devInfo));
+    exit(1);
   }
   memcpy(&devInfo, buff, sizeof (devInfo));
   printBuffer("display info", buff, b);
@@ -234,6 +340,13 @@ int main() {
   clear_buff(buff, 65);
   printf("Horiz: %u\nVert: %u\nHz: %u\n", devInfo.h_resolution, devInfo.v_resolution, devInfo.refreshRate);
   printf("Unk0: %u\nUnk1: %d\nUnk2: %d\nUnk3: %d\nUnk4: %u\n", devInfo.unk0, devInfo.unk1, devInfo.unk2, devInfo.unk3, devInfo.unk4);
+
+  b = get_report(hid_hmd, 0x01, buff, 43);
+  if (b < 0) {
+    printf("Failed to read report 1\n");
+    exit(1);
+  }
+  printBuffer("report 1", buff, b);
 
   buff[0] = 0x07;
   buff[1] = 0xa3;
@@ -244,21 +357,18 @@ int main() {
   buff[1] = 0x00;
   hid_send_feature_report(hid_hmd, buff, 2); // Disables HMD
 
-  clear_buff(buff, 65);
+  /* Dump firmware blocks. Higher blocks don't have anything, some lower blocks crash the headset */
+  dump_fw_block(hid_hmd, 1);
+  for (int blk = 9; blk < 0x13; blk++)
+    dump_fw_block(hid_hmd, blk);
+
   buff[0] = 0x14;
   buff[1] = 0x01;
-
   hid_send_feature_report(hid_hmd, buff, 2); // Enables Camera Device
-
-  clear_buff(buff, 65);
-  printf("Sending 0x0A 0x02\n");
 
   buff[0] = 0x0A;
   buff[1] = 0x02;
-
   hid_send_feature_report(hid_hmd, buff, 2); // Turn on Wireless
-
-  clear_buff(buff, 65);
 
   buff[0] = 0x02;
   buff[1] = 0x01;
